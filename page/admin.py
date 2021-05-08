2f52d180963d4a0e3f7c58352b3c88af5c600ec1
Onur <onrozcan20@gmail.com>
2021-05-09 00:50:00 +0300

    Carousel model added

---
+++ page/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Page, Carousel
+
+class PageModify(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'status',
+        'updated_at',
+    )
+
+    list_filter = ('status',)
+    list_editable = ('status','title',)
+
+
+admin.site.register(Page, PageModify)
+admin.site.register(Carousel)
+
+
---
+++ page/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+DEFAULT_STATUS = "draft"
+
+STATUS=[
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    ('deleted', 'Deleted'),
+]
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200, default="",)
+    content = models.TextField()
+    cover_image=models.ImageField(
+        upload_to='page',
+        null=True,
+        blank=True,
+    )
+    status= models.CharField(
+        default="DEFAULT_STATUS",
+        choices=STATUS,
+        max_length=10,
+    )
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class Carousel(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    cover_image = models.ImageField(
+        upload_to='carousel',
+        null=True,
+        blank=True,
+    )
+    status = models.CharField(
+        default="DEFAULT_STATUS",
+        choices=STATUS,
+        max_length=10
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
